c25f80dcde4b54adc4106a0184784b28ce408195
Joseph Monton <35918470+JMontskie@users.noreply.github.com>
2022-10-21 19:38:07 +0800

    updated files

---
+++ index.js
@@ -0,0 +1,21 @@
+// #STEPS IN INSTALLING EXPRESS\
+// mkdir myapp
+// cd myapp
+
+// npm init
+
+// npm install express
+
+add .gitignore 
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
